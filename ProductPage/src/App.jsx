4995e7ba50cb8e35737d37abfed3201e4c637b22
THalsell <139520247+THalsell@users.noreply.github.com>
2024-06-10 19:24:34 -0500

    created components

---
+++ ProductPage/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ProductDetail from './components/ProductDetail'
+import ProductList from './components/ProductList'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <ProductDetail />
+    <ProductList />
+    </>
+  )
+}
+
+export default App
